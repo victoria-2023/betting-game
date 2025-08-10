2df5985a457a2f19ca1ee86b032c5f305fc6320a
Victoria <victoriamarachi2001@gmail.com>
2025-08-10 23:56:27 +0300

    Fix CORS configuration: disable credentials to allow wildcard origins

--- src/main/java/com/bettinggame/config/CorsConfig.java
+++ src/main/java/com/bettinggame/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
